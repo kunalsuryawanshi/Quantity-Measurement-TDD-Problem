2871b279a0690ce0d1d0a57d6ccaf5e1b41cfa31
kunalsuryawanshi <kunalsuryawanshi25@gmail.com>
2021-10-30 00:07:53 +0530

    Added: Given String and Int Data for Failing Test Case

--- src/main/java/com/quantitymesurement/QuantityMeasurement.java
+++ src/main/java/com/quantitymesurement/QuantityMeasurement.java
@@ -9,7 +9,7 @@ public class QuantityMeasurement {
      * @pram: length1, length2
      * return: boolean value
      */
-    public boolean compareLengths(Double length1,Double length2) {
+    public boolean compareLengths(Object length1,Object length2) {
         return length1.equals(length2);
     }
     /*
@@ -18,7 +18,14 @@ public class QuantityMeasurement {
      * return: boolean value
      */
     public boolean nullCheck(Double length1,Double length2) {
-        boolean flag = length1 == null || length2 == null;
-        return flag;
+        return length1 == null || length2 == null;
+    }
+    /*
+     * Purpose : To check given feet contains which data type
+     * @pram: length1, length2
+     * return: boolean type
+     */
+    public boolean compareDataType(Object length1,Object length2) {
+        return length1.getClass() == length2.getClass();
     }
 }
--- src/test/java/com/quantitymesurement/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymesurement/QuantityMeasurementTest.java
@@ -9,7 +9,7 @@ public class QuantityMeasurementTest {
     // Given Two feet when not Equal Should return False
     @Test
     public void givenTwoFeet_WhenNotEqual_ShouldReturnFalse() {
-        boolean res = quantityMeasurement.compareLengths(1.1,0.0);
+        boolean res = quantityMeasurement.compareLengths(0.0,16.0);
         Assertions.assertFalse(res);
     }
 
@@ -19,11 +19,37 @@ public class QuantityMeasurementTest {
         boolean res = quantityMeasurement.compareLengths(0.0,0.0);
         Assertions.assertTrue(res);
     }
-    // Given feet When null should return True
+    /*
+     * 1: Null Check
+     * Given feet When null should return True
+     */
     @Test
     public void givenFeet_WhenNull_ShouldReturnTrue() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
         boolean res = quantityMeasurement.nullCheck(null,null);
         Assertions.assertTrue(res);
+    }
+    @Test
+    public void givenOneFeet_WhenNull_ShouldReturnTrue() {
+        boolean res = quantityMeasurement.nullCheck(null,2.0);
+        Assertions.assertTrue(res);
+    }
+    @Test
+    public void givenFeet_WhenNotNull_ShouldReturnFalse() {
+        boolean res = quantityMeasurement.nullCheck(5.0,5.0);
+        Assertions.assertFalse(res);
+    }
+    /*
+     * 3: Type Check
+     * Given Feet When Different Data Type Should Return False
+     */
+    @Test
+    public void givenFeet_WhenIntegerType_ShouldReturnFalse() {
+        boolean res = quantityMeasurement.compareDataType(1, 1.0);
+        Assertions.assertFalse(res);
+    }
+    @Test
+    public void givenFeet_WhenStringType_ShouldReturnFalse() {
+        boolean res = quantityMeasurement.compareDataType("9", 1.0);
+        Assertions.assertFalse(res);
     }
 }
