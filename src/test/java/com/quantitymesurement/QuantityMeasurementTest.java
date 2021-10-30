d9f3552040c425ce4b102fc8e9972a8401b3af61
kunalsuryawanshi <kunalsuryawanshi25@gmail.com>
2021-10-31 00:26:10 +0530

    Refactored: Code and Test For Value Check for Equality

--- src/test/java/com/quantitymesurement/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymesurement/QuantityMeasurementTest.java
@@ -31,4 +31,18 @@ public class QuantityMeasurementTest {
         boolean res = feet.equals(1);
         Assertions.assertFalse(res);
     }
+
+    @Test
+    public void givenDifferentValueFeet1AndFeet2_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(8.0);
+        Feet feet2 = new Feet(11.0);
+        Assertions.assertNotEquals(feet1,feet2);
+    }
+
+    @Test
+    public void givenSameValueFeet1AndFeet2_ShouldReturnEqual() {
+        Feet feet1 = new Feet(8.0);
+        Feet feet2 = new Feet(8.0);
+        Assertions.assertEquals(feet1,feet2);
+    }
 }
